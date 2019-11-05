b5c94998fca87eea0adb81131f851124f19f4e28
Tyler Healey <tyler.healey@gmail.com>
2019-11-05 10:48:20 -0500

    Applied database migration for initial models

--- movietheatre/boxoffice/apps.py
+++ movietheatre/boxoffice/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class BoxofficeConfig(AppConfig):
-    name = 'boxoffice'
+    name = 'movietheatre.boxoffice'
--- movietheatre/settings.py
+++ movietheatre/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'boxoffice.apps.BoxofficeConfig'
+    'movietheatre.boxoffice',
 ]
 
 MIDDLEWARE = [
